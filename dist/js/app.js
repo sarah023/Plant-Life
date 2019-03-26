7f556e624fb377b8c1d03e8d660329e4a1ffdb61
Sarah Grundy <sarahgrundy023@gmail.com>
2019-03-26 13:42:01 +1030

    js started. toggle nav added

---
+++ dist/js/app.js
@@ -0,0 +1,9 @@
+const mainTag = document.querySelector('main');
+const toggleTag = document.querySelector('a toggle-nav');
+
+// //when toggleTag is clicked - toggle a class of 'open' onto the main tag
+// toggleTag.addEventListener('click', function() {
+//   mainTag.classList.toggle('open');
+// });
+
+toggleTag.innerHTML('yoyoyoyoyo');
